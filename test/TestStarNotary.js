f7947892b8507e6ca08566f341dd499d966e9b90
Antonio Furtado <antoniordf@hotmail.com>
2023-04-06 17:01:56 +0100

    Wrote test for transferStar

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -99,8 +99,16 @@ it("lets 2 users exchange stars", async () => {
 
 it("lets a user transfer a star", async () => {
   // 1. create a Star with different tokenId
+  let instance = await StarNotary.deployed();
+  let user1 = accounts[1];
+  let user2 = accounts[2];
+  let starId = 9;
+  await instance.createStar("awesome star", starId, { from: user1 });
   // 2. use the transferStar function implemented in the Smart Contract
+  await instance.transferStar(user2, starId, { from: user1 });
   // 3. Verify the star owner changed.
+  let newOwner = await instance.ownerOf(starId);
+  assert.equal(newOwner, user2);
 });
 
 it("lookUptokenIdToStarInfo test", async () => {
